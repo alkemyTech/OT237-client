6d7743d273e0cbf6f4458b4f394e4f11b4f8aaa7
Nicolás Chiesa <nicochiesa01@gmail.com>
2022-06-18 16:46:26 -0300

    CKEditor installed. To fix.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CKEditorModule } from "@ckeditor/ckeditor5-angular";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
-    FeaturesModule
+    FeaturesModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
