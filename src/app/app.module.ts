de1e7cc7ff757fc8e4754a31646be44c2390d46d
Nicolás Chiesa <nicochiesa01@gmail.com>
2022-06-18 17:08:21 -0300

    CKEditor correctly installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CKEditorModule } from "@ckeditor/ckeditor5-angular";
+
 
 @NgModule({
   declarations: [
@@ -13,8 +13,7 @@ import { CKEditorModule } from "@ckeditor/ckeditor5-angular";
   imports: [
     BrowserModule,
     CoreModule,
-    FeaturesModule,
-    CKEditorModule
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/app-routing.module.ts
+++ src/app/features/app-routing.module.ts
@@ -2,11 +2,17 @@ import { ActivityFormComponent } from "./pages/activities/activity-form/activity
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
+import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 
 const routes: Routes = [
   { 
     path: "actividades", 
-    component: ActivityFormComponent },
+    component: ActivityFormComponent 
+  },
+  {
+    path: "news",
+    component: NewsFormComponent
+  },
   {
     path: "",
     redirectTo: "actividades",
@@ -16,7 +22,7 @@ const routes: Routes = [
     path: "**",
     redirectTo: "actividades",
     pathMatch: "full",
-  },
+  }
 ];
 
 @NgModule({
