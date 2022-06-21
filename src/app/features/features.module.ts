21c63b5d077df82c4edb02a9a8614d8a3fce5b74
PabloGCH <pablochoconi@gmail.com>
2022-06-21 17:18:58 -0300

    added FormModule and ReactiveFormModule modules to features.module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -10,6 +10,7 @@ import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.component";
 import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -33,6 +34,6 @@ import { UserFormComponent } from "./pages/users/user-form/user-form.component";
     UserFormComponent,
     RouterModule
   ],
-  imports: [CommonModule, AppRoutingModule, RouterModule],
+  imports: [CommonModule, AppRoutingModule, RouterModule, FormsModule, ReactiveFormsModule],
 })
 export class FeaturesModule {}
