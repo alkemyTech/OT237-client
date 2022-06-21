858de75348b53a442dc761c62b73f73fac3310dc
PabloGCH <pablochoconi@gmail.com>
2022-06-21 17:24:33 -0300

    Added html, logic and style to login-form

--- src/app/features/pages/auth/login-form/login-form.component.ts
+++ src/app/features/pages/auth/login-form/login-form.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+	selector: 'app-login-form',
+	templateUrl: './login-form.component.html',
+	styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
+	loginForm = new FormGroup({
+		email: new FormControl('', [
+			Validators.required,
+			Validators.email
+		]),
+		password: new FormControl('', [
+			Validators.required,
+			Validators.minLength(6),
+		])
+	});
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+	
+	}
+	emailError(input :string) {
+		return this.loginForm.get(input)?.errors?.['email'];
+	}
+	requiredError(input :string) {
+		return this.loginForm.get(input)?.errors?.['required'];
+	}
+	minLengthError(input :string) {
+		return this.loginForm.get(input)?.errors?.['minlength'];
+	}
+	invalid(input :string) {
+		return this.loginForm.get(input)?.invalid;
+	}
+	touched(input :string) {
+		return this.loginForm.get(input)?.touched;
+	}
+	
 }
