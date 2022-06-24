a731e4a3b926317821dcac3dfa38331810867d3e
PabloGCH <pablochoconi@gmail.com>
2022-06-24 20:57:24 -0300

    Form now validates for required fields on submit

--- src/app/features/pages/backoffice/organization-form/organization-form.component.ts
+++ src/app/features/pages/backoffice/organization-form/organization-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
@@ -9,9 +10,44 @@ import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
 export class OrganizationFormComponent implements OnInit {
 	public shortDescEditor = ClassicEditorBuild;
 	public longDescEditor = ClassicEditorBuild;
-	constructor() { }
-
-	ngOnInit(): void {
+	public orgEditForm :FormGroup;
+	public submitted :boolean;
+	constructor() {
+		this.orgEditForm = this.buildForm();
+		this.submitted = false;
 	}
 
+	ngOnInit(): void {
+
+	}
+	public submit() :void{
+		console.log("submitted");
+		this.submitted = true;
+	}
+	public isSubmitted() :boolean {
+		return this.submitted;
+	}
+	public requiredError(inputName :string) :boolean | null | undefined{
+		return this.orgEditForm.get(inputName)?.errors?.['required'];
+	}
+
+	private buildForm() :FormGroup{
+		return new FormGroup({
+			name: new FormControl('', [
+				Validators.required
+			]),
+			logo: new FormControl('', [
+				Validators.required
+			]),
+			shortDesc: new FormControl('', [
+				Validators.required
+			]),
+			longDesc: new FormControl('', [
+				Validators.required
+			]),
+			linksInput: new FormControl('', [
+				Validators.required
+			])
+		});
+	}
 }
