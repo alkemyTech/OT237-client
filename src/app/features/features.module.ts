b10987fa64015696e076874e08f7021ffb7e77a3
eddie06 <ejhuancahuire@gmail.com>
2022-06-24 16:28:01 -0500

    Admin Section - Home Data Update

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
@@ -33,6 +34,6 @@ import { UserFormComponent } from "./pages/users/user-form/user-form.component";
     UserFormComponent,
     RouterModule
   ],
-  imports: [CommonModule, AppRoutingModule, RouterModule],
+  imports: [CommonModule, AppRoutingModule, RouterModule, ReactiveFormsModule],
 })
 export class FeaturesModule {}
--- src/app/features/pages/slides/slides-form/slides-form.component.ts
+++ src/app/features/pages/slides/slides-form/slides-form.component.ts
@@ -1,4 +1,6 @@
+import { SlideService } from './../../../../core/services/slide.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-slides-form',
@@ -6,10 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slides-form.component.scss']
 })
 export class SlidesFormComponent implements OnInit {
+  form!: FormGroup;
+  iterator: number = 0;
+  pages: string[] = ["Presentation", "Slide 1", "Slide 2", "Slide 3"];
+  fieldSlides: any = {
+    text: ['', [Validators.required]],
+    image: ['', [Validators.required, Validators.pattern('(https?\:\/\/)?(www\.)?([a-z0-9]([a-z0-9]|(\-[a-z0-9]))*\.)+[a-z]+(\/[\-a-z_]+)*(\/[a-zA-Z0-9]+\.(jpg|png|jpeg|JPG|PNG|JPEG){1})')]]
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder, 
+    private slideSvc: SlideService
+    ) {
+    this.form = this.fb.group({
+      welcomeText: ['', [Validators.required, Validators.minLength(20)]],
+      slide1: this.fb.group(this.fieldSlides),
+      slide2: this.fb.group(this.fieldSlides),
+      slide3: this.fb.group(this.fieldSlides),
+    });
   }
 
+  ngOnInit(): void {
+    this.slideSvc.getSlides().subscribe({
+      next: (res: any) => {
+        let slides = res.data;
+        for(let i=0; i<slides.length; i++) {
+          let slideField = `slide${i+1}`;
+          this.form.get(slideField)?.get('text')?.setValue(slides[i].name);
+          this.form.get(slideField)?.get('image')?.setValue(slides[i].image);
+        }
+      },
+    });
+  }
+
+  get welcomeText() {
+    return this.form.get('welcomeText');
+  }
+
+  get textBySlide() {
+    return this.form.get('slide' + this.iterator)?.get('text');
+  }
+
+  get imageBySlide() {
+    return this.form.get('slide' + this.iterator)?.get('image');
+  }
+
+  checkControl(control: any, type: string): boolean {
+    return control.hasError(type) && (control.touched || control.dirty);
+  }
+
+  onNextPage() {
+    if(this.iterator == this.pages.length - 1) {
+      if(this.form.invalid) {
+        return;
+      }
+      console.log(this.form.value);
+      this.form.reset();
+      this.iterator = 0;
+    } else this.iterator++;
+  }
+
+  onPrevPage() {
+    if(this.iterator == 0) return;
+    this.iterator--;
+  }
 }
