391e0b66cc3f90bc43a4413d72292bb572edfa44
PabloGCH <pablochoconi@gmail.com>
2022-06-24 21:55:16 -0300

    File and url validations added

--- src/app/features/pages/backoffice/organization-form/organization-form.component.ts
+++ src/app/features/pages/backoffice/organization-form/organization-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms'
+import {FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors} from '@angular/forms'
 import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
@@ -21,7 +21,6 @@ export class OrganizationFormComponent implements OnInit {
 
 	}
 	public submit() :void{
-		console.log("submitted");
 		this.submitted = true;
 	}
 	public isSubmitted() :boolean {
@@ -30,14 +29,36 @@ export class OrganizationFormComponent implements OnInit {
 	public requiredError(inputName :string) :boolean | null | undefined{
 		return this.orgEditForm.get(inputName)?.errors?.['required'];
 	}
+	public urlError(inputName :string) :boolean | null | undefined {
+		return this.orgEditForm.get(inputName)?.errors?.['url'];
+	}
+	public patternError(inputName :string) :boolean | null | undefined {
+		return this.orgEditForm.get(inputName)?.errors?.pattern;
+	}
+	public fileError(inputName :string) :boolean | null | undefined {
+		return this.orgEditForm.get(inputName)?.errors?.['fileInvalid'];
+	}
 
+
+	private fileValidation() :ValidatorFn{
+		return (control :AbstractControl) :ValidationErrors | null =>{
+			const pathArr :Array<string> = control.value.split(".");
+			const ext :string = pathArr[pathArr.length - 1];
+			if(ext == "png" || ext == "jpg"){
+				return null;
+			} else {
+				return {fileInvalid: true};
+			}
+		}
+	}
 	private buildForm() :FormGroup{
 		return new FormGroup({
 			name: new FormControl('', [
 				Validators.required
 			]),
 			logo: new FormControl('', [
-				Validators.required
+				Validators.required,
+				this.fileValidation()
 			]),
 			shortDesc: new FormControl('', [
 				Validators.required
@@ -46,7 +67,8 @@ export class OrganizationFormComponent implements OnInit {
 				Validators.required
 			]),
 			linksInput: new FormControl('', [
-				Validators.required
+				Validators.required,
+				Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
 			])
 		});
 	}
