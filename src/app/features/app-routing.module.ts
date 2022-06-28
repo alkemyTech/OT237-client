28ffc7227f18c3dadff5f48045684b5b16cd8137
Alexis Ocampo <alexisocampo@MacBook-Pro-de-Alexis.local>
2022-06-28 16:37:31 -0300

    basic contact page added, page-title component created, phone pipe created

--- src/app/features/app-routing.module.ts
+++ src/app/features/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { ActivityFormComponent } from "./pages/activities/activity-form/activity-form.component";
 import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
-
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
--- src/app/features/pages/contact/contact/contact.component.ts
+++ src/app/features/pages/contact/contact/contact.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  pageTitle: string = 'Contacto'
+
+  email: string = 'somosfundacionmas@gmail.com'
+  instagram: string = 'SomosMas'
+  facebook: string = 'Somos Más'
+  phone: string = '1160112988'
 
   constructor() { }
 
