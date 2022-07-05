ffd636df70c65c88b14306249204853b5d79a784
Nicolás Chiesa <nicochiesa01@gmail.com>
2022-07-05 20:46:11 -0300

    Core module fixed

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,6 @@
 import { SlideService } from './services/slide.service';
 import { RoleService } from './services/role.service';
 import { UserService } from './services/user.service';
-import { SlideService } from './services/slide.service';
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import {
