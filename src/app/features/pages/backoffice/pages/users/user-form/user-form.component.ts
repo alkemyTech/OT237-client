15ba35b566f099a53d409e2b49c2e84c101b19fa
Martin Cordero <martin.ariel.cordero76@gmail.com>
2022-07-25 15:49:31 -0300

    fix router

--- src/app/features/pages/backoffice/pages/users/user-form/user-form.component.ts
+++ src/app/features/pages/backoffice/pages/users/user-form/user-form.component.ts
@@ -123,7 +123,7 @@ export class UserFormComponent implements OnInit {
             text: `El usuario ${form.name} se creó correctamente`,
             icon: 'success',
           }).then(() => {
-            this.router.navigateByUrl('backoffice/users/list');
+            this.router.navigateByUrl('backoffice/user/list');
           });
         } else {
           Swal.fire({
